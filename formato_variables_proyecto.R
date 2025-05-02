bc19ebb84abcfd93d73c04e9334d0075434bb435
Ricardo Ahumada O. <r.ahumada.oliva@gmail.com>
2025-05-02 15:39:23 -0400

    creacion de archivo de formato para las variables del proyecto

---
+++ formato_variables_proyecto.R
@@ -0,0 +1,21 @@
+# 1. Rutas para archivo de dimensiones o dims ----
+
+ruta_dim_establecimientos <- ''
+ruta_dim_abreviaciones <- ''
+ruta_dim_especialidades <- ""
+ruta_dim_causales <- ''
+ruta_dim_actividades_programacion <- ''
+ruta_dim_inscripcion_APS <- ''
+
+# 2. Rutas para archivos de hechos o facts ----
+
+ruta_fact_programacion_carpeta <- ''
+ruta_fact_programacion_hblt <- paste0(ruta_fact_programacion_carpeta,'/HBLT.xlsx')
+ruta_fact_programacion_hegc <- paste0(ruta_fact_programacion_carpeta,'/HEGC.xlsx')
+ruta_fact_programacion_hpino <- paste0(ruta_fact_programacion_carpeta,'/HPINO.xlsx')
+ruta_fact_programacion_hslbp <- paste0(ruta_fact_programacion_carpeta,'/HSLBP.xlsx')
+ruta_fact_programacion_hdlc <- paste0(ruta_fact_programacion_carpeta,'/HDLC.xlsx')
+
+ruta_fact_le_abierta <- ''
+ruta_fact_le_cerrada <- ''
+ruta_fact_produccion_rem_deis <- ''
